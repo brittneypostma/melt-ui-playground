5b60997db4712a6f6e2d916107e4b97fba05e31b
abdel-17 <abdelrahmanb015@gmail.com>
2023-12-27 15:31:50 +0200

    fix: tailwind errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
-import type { Config } from 'tailwindcss';
-// import { craftPlugin } from '@getprovi/craft/dist/tailwind/craft-plugin.js';
 import { craftPreset } from '@getprovi/craft/dist/tailwind/craft-preset.js';
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'./node_modules/@getprovi/craft/**/*.{html,js,svelte,ts}'
 	],
-	// plugins: [craftPlugin]
+	theme: {
+		extend: {
+			zIndex: defaultTheme.zIndex
+		}
+	},
 	presets: [craftPreset]
 } satisfies Config;
